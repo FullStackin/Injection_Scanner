19aa57d9efbb7116338e5c065d5231cc891ab111
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-11-09 13:41:51 -0800

    adjusted code a bit(minor mistake)

--- scan.py
+++ scan.py
@@ -22,9 +22,14 @@ def form_details(form):
 
     # Loop through input tags in the form
     for input_tag in form.find_all("input"):
-        input_type = input_tag.attrs.get("text", "text")
+        input_type = input_tag.attrs.get("type", "text")
         input_name = input_tag.attrs.get("name")
         input_value = input_tag.attrs.get("value", "")
+        inputs.append({
+            "type": input_type,
+            "name": input_name,
+            "value": input_value,
+        })
 
     # Populate details of the form
     detailsOfForm['action'] = action
@@ -34,11 +39,10 @@ def form_details(form):
 
 # Function to check if the response indicates a SQL injection vulnerability
 def vulnerable(response):
-    errors = {
-        "quoted string not properly terminated",
-        "unclosed quotation mark after the character string",
-        "you have an error in your SQL syntax"
-    }
+    errors = {"quoted string not properly terminated",
+              "unclosed quotation mark after the charachter string",
+              "you have an error in you SQL syntax"
+              }
     for error in errors:
         if error in response.content.decode().lower():
             return True
@@ -53,32 +57,34 @@ def sql_injection_scan(url):
     for form in forms:
         details = form_details(form)
 
-    # Loop through double and single quote characters
-    for i in "\"'":
-        data = {}
-        # Loop through input tags in the form to create payload
-        for input_tag in details["inputs"]:
-            if input_tag["type"] == "hidden" or input_tag["value"]:
-                data[input_tag['name']] = input_tag["value"] + i
-            elif input_tag["type"] != "submit":
-                data[input_tag['name']] = f"test{i}"
+        # Loop through double and single quote characters
+        for i in "\"'":
+            data = {}
+            # Loop through input tags in the form to create payload
+            for input_tag in details["inputs"]:
+                if input_tag["type"] == "hidden" or input_tag["value"]:
+                    data[input_tag['name']] = input_tag["value"] + i
+                elif input_tag["type"] != "submit":
+                    data[input_tag['name']] = f"test{i}"
 
-        # Print the URL and form details
-        print(url)
-        form_details(form)
+            # Print the URL and form details
+            print(url)
+            form_details(form)
 
-        # Make a request based on the form method (POST or GET)
-        if details["method"] == "post":
-            res = s.post(url, data=data)
-        elif details["method"] == "get":
-            res = s.get(url, params=data)
+            # Make a request based on the form method (POST or GET)
+            if details["method"] == "post":
+                res = s.post(url, data=data)
+            elif details["method"] == "get":
+                res = s.get(url, params=data)
 
-        # Check for SQL injection vulnerability in the response
-        if vulnerable(res):
-            print("SQL injection attack vulnerability in link:", url)
-            break
+            # Check for SQL injection vulnerability in the response
+            if vulnerable(res):
+                print("SQL injection attack vulnerability in link:", url)
+            else:
+                print("No SQL injection attack vulnerability detected")
+                break
 
 if __name__ == "__main__":
     # Specify the URL to be checked for SQL injection vulnerability
-    urlToBeChecked = "Enter website here"
+    urlToBeChecked = "Enter Website to scan here"
     sql_injection_scan(urlToBeChecked)
